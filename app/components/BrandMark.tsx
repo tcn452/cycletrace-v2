05553f19a00c6d3b8fceb0ee6b0b128ca3f4c16d
Timothy Chikoko <tcn452@gmail.com>
2026-09-14 13:25:19 +0200

    Use vector CycleTrace logo

--- app/components/BrandMark.tsx
+++ app/components/BrandMark.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export function BrandMark() {
-  return <span className="brand-logo" aria-hidden="true"><Image src="/cycletrace-logo.png" alt="" width={680} height={470} priority /></span>
+  return <span className="brand-logo" aria-hidden="true"><Image src="/logo.svg" alt="" width={508} height={498} priority /></span>
 }
